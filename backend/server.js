4f6e4610933e42e5d2aa2e25d746e1ade223068c
NiharikaGoel02 <goelniharika2004@gmail.com>
2026-02-01 14:17:51 +0530

    Updated backend repo

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://code-mind-peach.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 const connectDB = require('./config/db');
 
 connectDB();
